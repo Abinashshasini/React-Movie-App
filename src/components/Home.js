14b683a729d3c64dfdc967f602f6a43439cef74c
Abinash Shasini <abinashshasini125@gmail.com>
2021-07-19 17:41:19 +0530

    second commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
-import { concat } from "async";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import API from "../API";
 //config
 import { IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from "../config";
-//Components
 
+//Components
+import HeroImage from "./HeroImage";
+import Grid from "./Grid";
+import Thumb from "./Thumb";
+import Spinner from "./Spinner";
 //Hook
 import { useHomeFetch } from "../hooks/useHomeFetch";
 
@@ -15,7 +18,32 @@ const Home = () => {
   const { state, loading, error } = useHomeFetch();
   console.log(state);
 
-  return <div>Home Page</div>;
+  return (
+    <>
+      {state.results[0] && (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+      )}
+      <Grid header="Popular Movies">
+        {state.results.map((movie) => (
+          <Thumb
+            key={movie.id}
+            clickable
+            image={
+              movie.poster_path
+                ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                : NoImage
+            }
+            movieId={movie.id}
+          />
+        ))}
+      </Grid>
+      <Spinner />
+    </>
+  );
 };
 
 export default Home;
